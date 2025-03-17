82134fea1393a97a294b7b03145523e3cd014794
Puwebserver <dymarbiz@gmail.com>
2025-03-17 10:22:49 +0200

    Create vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react()],
+    base: '/video-editor/' // Adjust this to match your GitHub repo name
+});
